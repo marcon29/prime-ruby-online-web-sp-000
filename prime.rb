3d50649fd34b5f1cb4dabae04b10704a84a6b360
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 23:41:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(n)
   if n.even? || n <= 1
     false
   else
-    array = (3..n-1).to_a.select{|num| num.odd?}
+    array = (2..n-1).to_a.select{|num| num.odd?}
     array.none?{|num| n%num == 0}
   end
 end
