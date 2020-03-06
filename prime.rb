6b4c228d0968215a839a1a99f3f60ade665c69a0
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 23:36:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,5 +6,13 @@ def prime?(n)
   end
 end
 
+array.any?{|num| n%num == 0}
+
+def prime(n)
+  array = (2..n-1).to_a.select{|num| num.odd?}
+  array.none?{|num| n%num == 0}
+end
+
+
 puts prime?(9).inspect
 puts prime?(11).inspect
