348b131782fa714486cb5f1c70c99185714a0797
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 23:11:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,7 +8,9 @@ end
   #(1..n).to_a
 end
 
+puts prime?(6)
 puts prime?(5)
+puts prime?(-1)
 
 #  collection = (1..n).to_a.collect{ |num| num.odd? }
 # collection.collect{|arg| code to execute using arg}
