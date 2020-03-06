7f89dca97970ba1c02ac64dced050e3e62aa5569
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 23:54:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,8 @@
 def prime?(n)
-  if (n > 2 && n.even?) || n <= 1
-    false
-  else
-    array = (2..n-1).to_a.select{|num| num.odd?}
-    array.none?{|num| n%num == 0}
-  end
+ if (n > 2 && n.even?) || n <= 1
+   false
+ else
+   array = (2..n-1).to_a.select{|num| num.odd?}
+   array.none?{|num| n%num == 0}
+ end
 end
-
-# puts prime?(2)
-# puts prime?(3)
-# puts prime?(4)
-# puts prime?(5)
-# puts prime?(9)
-# puts prime?(11)
-# puts prime?(105557)
