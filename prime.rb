4221dcc4bfab75754714b32a14b0a306f654ccaa
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 23:02:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 def prime?(n)
-  collection = (1..n).to_a.collect{ |num| num.odd? }
+  (1..n).to_a.collect do |num|
+    array << num if num.odd?
+  end 
 end
 
 puts prime?(5)
 
+#  collection = (1..n).to_a.collect{ |num| num.odd? }
 # collection.collect{|arg| code to execute using arg}
