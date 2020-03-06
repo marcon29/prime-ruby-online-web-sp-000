4cbe52207370379f7cea01111b964d7ef14818c8
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 23:39:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,17 +2,10 @@ def prime?(n)
   if n.even? || n <= 1
     false
   else
-    array = (1..n).to_a.select{|num| num.odd?}
+    array = (3..n-1).to_a.select{|num| num.odd?}
+    array.none?{|num| n%num == 0}
   end
 end
-
-array.any?{|num| n%num == 0}
-
-def prime(n)
-  array = (2..n-1).to_a.select{|num| num.odd?}
-  array.none?{|num| n%num == 0}
-end
-
 
 puts prime?(9).inspect
 puts prime?(11).inspect
